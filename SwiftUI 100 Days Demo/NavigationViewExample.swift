074f2651030ba2980ad82072a9af4be97248fd1a
飞鱼 <870027381@qq.com>
2021-08-29 11:31:58 +0800

    为应用添加导航栏 NavigationView

---
+++ SwiftUI 100 Days Demo/NavigationViewExample.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationViewExample.swift
+//  SwiftUI 100 Days Demo
+//
+//  Created by FeiYu on 2021/8/29.
+//
+
+import SwiftUI
+
+struct NavigationViewExample: View {
+    var body: some View {
+        NavigationView {
+            FormExample()
+                .navigationBarTitle(Text("SwiftUI"))
+        }
+    }
+}
+
+struct NavigationViewExample_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationViewExample()
+    }
+}
