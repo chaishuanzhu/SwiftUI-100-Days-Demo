55f21fd7b743f1a7c4e01fdeb9e8413637242a69
飞鱼 <870027381@qq.com>
2021-08-29 11:10:32 +0800

    Day1 Form Example

---
+++ SwiftUI 100 Days Demo/FormExample.swift
@@ -0,0 +1,28 @@
+//
+//  FormExample.swift
+//  SwiftUI 100 Days Demo
+//
+//  Created by FeiYu on 2021/8/29.
+//
+
+import SwiftUI
+
+struct FormExample: View {
+    var body: some View {
+        Form {
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+            Text("Hello World")
+        }
+    }
+}
+
+struct FormExample_Previews: PreviewProvider {
+    static var previews: some View {
+        FormExample()
+    }
+}
